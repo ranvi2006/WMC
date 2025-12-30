95c9659be739e9bb96402684f06ee7e2d0c83755
ranvi2006 <rajurk2002@gmail.com>
2025-12-30 09:44:29 +0530

    Basic Satup Done

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');     
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());     
+
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.all(/.*/, (req, res) => {
+    res.status(404).send({ message: `Route ${req.originalUrl} not found.` });
+});
+
+module.exports = app;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <center><h1>We Make Coder</h1></center>
+    </>
+  )
+}
+
+export default App
