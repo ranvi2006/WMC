7923c7b49f1a04dd27360560adcd73f55f50b303
ranvi2006 <rajurk2002@gmail.com>
2026-01-03 08:27:03 +0530

    did some chenged in home page css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import "../styles/home.css";
+import "../styles/Home.css";
 
 export default function Home() {
   const { isAuthenticated } = useSelector((state) => state.auth);
