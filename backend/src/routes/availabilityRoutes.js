3758c7d15dfb8d6360ac4cd470c0f5cc889f7634
Raju kumar mandal <rajurk2002@gmail.com>
2026-05-17 17:22:27 +0530

    Updated mores

--- backend/src/routes/availabilityRoutes.js
+++ backend/src/routes/availabilityRoutes.js
@@ -1,15 +1,21 @@
 const express = require("express");
+
 const {
   setAvailability,
-  getAvailability,
   getAvailability,
   autoCreateSlots,
   adminCreateSlots,
 } = require("../controllers/availabilityController");
+
 const { isAuthenticated } = require("../middlewares/authMiddleware");
+
 const { allowRoles } = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
+
+/* =========================================================
+   GET AVAILABILITY
+========================================================= */
 
 router.get(
   "/",
@@ -17,20 +23,27 @@ router.get(
   allowRoles("teacher", "admin", "student"),
   getAvailability,
 );
+
+/* =========================================================
+   SET AVAILABILITY
+========================================================= */
+
 router.post("/", isAuthenticated, allowRoles("teacher"), setAvailability);
 
-router.post(
-  "/getAll",
-  isAuthenticated,
-  allowRoles("student", "teacher", "admin"),
-  getAvailability,
-);
+/* =========================================================
+   AUTO CREATE SLOTS
+========================================================= */
+
 router.post(
   "/auto-create",
   isAuthenticated,
   allowRoles("admin"),
   autoCreateSlots,
 );
+
+/* =========================================================
+   ADMIN CREATE SLOTS
+========================================================= */
 
 router.post(
   "/admin-create",
